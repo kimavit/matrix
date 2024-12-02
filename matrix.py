32017f03763fe013d52854eae78c09c55c0b5020
kmavit <kimavit@gmail.com>
2024-12-02 20:55:08 +0300

    matrix start

---
+++ matrix.py
@@ -0,0 +1,13 @@
+rows, cols = int(input()), int(input())
+matrix = [[0]*cols for _ in range (rows)]
+for i in range (rows):
+    for j in range (cols):
+        matrix [i][j] = input()
+for k in range (rows):
+    print (*matrix[k], end = "\n")
+print()
+    
+for j in range (cols):
+    for i in range (rows):
+        print (matrix[i][j], end = " ")
+    print ()
